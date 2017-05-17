c9c0c9a1812e59e658735f7e3ea5cf65caf0312c
Joe Fearnley <joe.fearnley@gmail.com>
2017-05-17 02:49:53 +0000

    Adding another test

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,10 @@ const assert = require('assert');
 
 describe('Test Suite', function() {
     it('should contain 23 letters when on 342 (three hundred and forty-two) ', () => {
-      assert.equal(23, containsNumberOfLetters(342));
-  });
+        assert.equal(23, containsNumberOfLetters(342));
+    });
+
+    it('should contain 20 letters when on 115 (one hundred and fifteen) ', () => {
+        assert.equal(2, containsNumberOfLetters(115));
+    });
 });
