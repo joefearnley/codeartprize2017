01401cf3c7de81a950b2bdc47b667dd0e575a36d
Joe Fearnley <joe.fearnley@gmail.com>
2017-05-17 02:48:29 +0000

    Adding test library and test files

---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Test Suite', function() {
+    it('should contain 23 letters when on 342 (three hundred and forty-two) ', () => {
+      assert.equal(23, containsNumberOfLetters(342));
+  });
+});
